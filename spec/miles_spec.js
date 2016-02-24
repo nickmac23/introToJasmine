39e07abbff3c99091c47495b6e522504b741dcb6
nickmac <nickpmacintyre@gmail.com>
2016-02-24 11:53:13 -0700

    one linner!

--- spec/miles_spec.js
+++ spec/miles_spec.js
@@ -1,7 +1,20 @@
 var miles = require('../lib/miles.js')
 
 describe('miles', function () {
-  it('is our master plan here', function () {
-    expect(miles.someFunction()).toEqual("running stuff")
+  it('determines if a car has enough gas to travel route', function () {
+      var car = {
+        milesPerGallon: 20,
+        gallonsInTank: 3
+      };
+
+      var route = {
+        miles: 200,
+        destination: "Boulder",
+        location: "Fancy Land"
+      };
+
+    expect(miles.hasEnoughGas(car, route)).toEqual(false)
+    car.gallonsInTank += 20
+    expect(miles.hasEnoughGas(car, route)).toEqual(true)
   })
-})
+});
