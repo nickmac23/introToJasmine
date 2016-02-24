b7fb5892ba3f2a767a235304dc966492f208bcf5
nickmac <nickpmacintyre@gmail.com>
2016-02-24 11:33:23 -0700

    first commit

---
+++ spec/miles_spec.js
@@ -0,0 +1,7 @@
+var miles = require('../lib/miles.js')
+
+describe('miles', function () {
+  it('is our master plan here', function () {
+    expect(miles.someFunction()).toEqual("running stuff")
+  })
+})
